649c652e3114547f66e93b5595fd78bbb8c057b5
Chalk <9481405+chalkpe@users.noreply.github.com>
2026-05-15 21:23:31 +0900

    fix: emoji admin

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,25 @@ const category = '텍모지'
 const host = process.env.MISSKEY_HOST
 if (!host) throw new Error('MISSKEY_HOST is not set')
 
-const token = process.env.MISSKEY_TOKEN
-if (!token) throw new Error('MISSKEY_TOKEN is not set')
+const botToken = process.env.MISSKEY_BOT_TOKEN
+if (!botToken) throw new Error('MISSKEY_BOT_TOKEN is not set')
 
-const client = new api.APIClient({ origin: host, credential: token })
+const emojiAdminToken = process.env.MISSKEY_EMOJI_ADMIN_TOKEN
+if (!emojiAdminToken) throw new Error('MISSKEY_EMOJI_ADMIN_TOKEN is not set')
+
+const botClient = new api.APIClient({ origin: host, credential: botToken })
+const emojiAdminClient = new api.APIClient({ origin: host, credential: emojiAdminToken })
 
 async function isEmojiAvailable(name: string) {
   try {
-    const emoji = await client.request('emoji', { name })
+    const emoji = await emojiAdminClient.request('emoji', { name })
     return !emoji
   } catch {
     return true
   }
 }
 
-const stream = new Stream(host, { token })
+const stream = new Stream(host, { token: botToken })
 const mainChannel = stream.useChannel('main')
 mainChannel.on('notification', async (notification) => {
   if (notification.type !== 'mention') return
@@ -41,16 +45,16 @@ mainChannel.on('notification', async (notification) => {
   if (await isEmojiAvailable(name)) {
     try {
       const file = await createEmoji(keyword)
-      const { id: fileId } = await client.request('drive/files/create', { name, file, comment: keyword })
+      const { id: fileId } = await botClient.request('drive/files/create', { name, file, comment: keyword })
 
-      await client.request('admin/emoji/add', { name, fileId, category, aliases })
-      await client.request('notes/reactions/create', { noteId, reaction: `:${name}:` })
-      await client.request('notes/create', { visibility, replyId: noteId, text: `:${name}: ${name} 커모지가 등록되었어요!` })
+      await emojiAdminClient.request('admin/emoji/add', { name, fileId, category, aliases })
+      await botClient.request('notes/reactions/create', { noteId, reaction: `:${name}:` })
+      await botClient.request('notes/create', { visibility, replyId: noteId, text: `:${name}: ${name} 커모지가 등록되었어요!` })
     } catch (error) {
       console.error(error)
-      await client.request('notes/create', { visibility, replyId: noteId, text: `실패했습니다... 관리자에게 문의하세요...` })
+      await botClient.request('notes/create', { visibility, replyId: noteId, text: `실패했습니다... 관리자에게 문의하세요...` })
     }
   } else {
-    await client.request('notes/create', { visibility, replyId: noteId, text: `:${name}: 이미 ${name} 커모지가 등록되어 있어요.` })
+    await botClient.request('notes/create', { visibility, replyId: noteId, text: `:${name}: 이미 ${name} 커모지가 등록되어 있어요.` })
   }
 })
