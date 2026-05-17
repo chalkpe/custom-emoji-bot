e81df3bff6db805761513474afa3ef2f5cd19536
Chalk <9481405+chalkpe@users.noreply.github.com>
2026-05-17 21:33:25 +0900

    feat: edge case

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,26 @@ import { draw } from './image.js'
 const prefix = 'ko_'
 const category = '텍모지'
 
+const jamoNames: Record<string, string> = {
+  ㄳ: '기역시옷', ㄵ: '니은지읒', ㄶ: '니은히읗',
+  ㄺ: '리을기역', ㄻ: '리을미음', ㄼ: '리을비읍',
+  ㄽ: '리을시옷', ㄾ: '리을티읕', ㄿ: '리을피읖',
+  ㅀ: '리을히읗', ㅄ: '비읍시옷',
+  ㄲ: '쌍기역', ㄸ: '쌍디귿', ㅃ: '쌍비읍', ㅆ: '쌍시옷', ㅉ: '쌍지읒',
+  ㄱ: '기역', ㄴ: '니은', ㄷ: '디귿', ㄹ: '리을',
+  ㅁ: '미음', ㅂ: '비읍', ㅅ: '시옷', ㅇ: '이응',
+  ㅈ: '지읒', ㅊ: '치읓', ㅋ: '키읔', ㅌ: '티읕',
+  ㅍ: '피읖', ㅎ: '히읗',
+  ㅏ: '아', ㅐ: '애', ㅓ: '어', ㅔ: '에', ㅗ: '오',
+  ㅜ: '우', ㅟ: '위', ㅚ: '외', ㅡ: '으', ㅣ: '이',
+  ㅑ: '야', ㅒ: '얘', ㅕ: '여', ㅖ: '예', ㅘ: '와',
+  ㅙ: '왜', ㅛ: '요', ㅝ: '워', ㅞ: '웨', ㅠ: '유', ㅢ: '의',
+}
+
+function expandJamo(text: string): string {
+  return [...text].map((ch) => jamoNames[ch] ?? ch).join('')
+}
+
 const host = process.env.MISSKEY_HOST
 if (!host) throw new Error('MISSKEY_HOST is not set')
 
@@ -18,7 +38,7 @@ const botClient = new api.APIClient({ origin: host, credential: botToken })
 const emojiAdminClient = new api.APIClient({ origin: host, credential: emojiAdminToken })
 
 function createPayload(comment: string) {
-  const pronunciation = comment.replaceAll(/\s/g, '')
+  const pronunciation = expandJamo(comment.replaceAll(/\s/g, ''))
   const aliases = comment === pronunciation ? [comment] : [comment, pronunciation]
 
   const name = prefix + romanize(pronunciation)
